8e1e8c09d987a149a9e7fe705980a76f1d2feb12
Kris <kris@sneakerz.org>
2022-01-05 22:40:03 +1100

    add imp and hyp formats

--- util/winearc.py
+++ util/winearc.py
@@ -13,7 +13,9 @@ jsontemplate = """{
     "extensions": ["#EXT#"],
     "install": {
         "method": "apt",
-        "packages": ["wine"]
+        "packages": ["wine"],
+        "tool":"#BIN#",
+        "container":"win32/#EXT#/#DIST#"#DEPBLOCK#
     },
     "pack": {
         "exe": "wine",
@@ -39,12 +41,15 @@ def main(argv):
     ap = argparse.ArgumentParser(description="Create a dos archiver profile (JSON config)")
     ap.add_argument("-e","--extension",required=True, help="file extension (without .), will be used as the profile name and archive payload too by default")
     ap.add_argument("-b", "--binary", help="archiver executable name. Default is whatever the extension is e.g. arj")
+    ap.add_argument("-u", "--unarchiver", help="unarchiver executable name. Default is whatever the extension is e.g. arj")
+    ap.add_argument("-d", "--distfile", help="Archive distfile from dos/*/??.zip")
     ap.add_argument("-n", "--name", help="Profile name, default is whatever the extension is in uppercase. e.g. arj -> ARJ")
     ap.add_argument("-m", "--msg", help="Archive message, default is whatever the extension is in uppercase. e.g. arj -> ARJ")
     ap.add_argument("-a", "--addfiles", default="a", help="The command the archiver uses to add files to an archive.")
     ap.add_argument("-x", "--extfiles", default="x", help="The command the archiver uses to extract files from an archive.")
     args = ap.parse_args(argv)
 
+    os.chdir('..')
     def_file = os.path.join("defs", args.extension + ".json")
     if os.path.isfile(def_file):
         print(f"{def_file} already exists, aborting.")
@@ -56,6 +61,13 @@ def main(argv):
         tool = os.path.join("tools", binary.upper() + ".EXE")
     else:
         tool = os.path.join("tools", binary)
+    
+    depblock = ""
+    binary2 = args.unarchiver
+    if not args.unarchiver:
+        binary2 = binary
+    else:
+        depblock = f",\n        \"dependencies\":[\"{binary2}\"]"
         
     if not os.path.isfile(tool):
         print(f"binary {tool} doesn't exist so maketest will fail later.")
@@ -74,6 +86,9 @@ def main(argv):
     config = config.replace("#ADD#", args.addfiles)
     config = config.replace("#EXTRACT#", args.extfiles)
     config = config.replace("#MSG#", msg)
+    config = config.replace("#DIST#", args.distfile)
+    config = config.replace("#BIN2#", binary2)
+    config = config.replace("#DEPBLOCK#", depblock)
 
     print(f'writing config: {def_file}')
     open(def_file,'w').write(config)
@@ -94,6 +109,9 @@ def main(argv):
     config = config.replace("#EXTRACT#", args.extfiles)
     config = config.replace("#MSG#", msg)
     config = config.replace("#BLOB#", blob)
+    config = config.replace("#DIST#", args.distfile)
+    config = config.replace("#BIN2#", binary2)
+    config = config.replace("#DEPBLOCK#", depblock)
 
     print(f'writing config w/blob this time: {def_file}')
     open(def_file,'w').write(config)
